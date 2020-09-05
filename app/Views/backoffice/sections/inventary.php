adf611a73c87b47ea3efa93c26893ceaf3e8549c
Alfred Ortega <omar.alfredo49@gmail.com>
2020-09-05 12:44:03 -0500

    Finish inventary mockup

--- app/Views/backoffice/sections/inventary.php
+++ app/Views/backoffice/sections/inventary.php
@@ -34,6 +34,7 @@
 
                   </div>
 
+                  <!-- finish -->
                   <div class="inv-update-table w-100 d-none">
 
                     <div class="card collapsed-card">
@@ -436,104 +437,123 @@
 
                   </div>
 
-                  <div class="inv-inv-table d-none">
+                  <div class="inv-inv-table w-100 d-none">
 
-                    <div class="row">
-                      <div class="col-6 float-left align-middle">
-                        <span>Total de activos: <b>XX</b> </span>
-                      </div>
-                      <div class="col-5 float-right">
-                        <label class="sr-only" for="searchActiveInv">Buscar</label>
-                        <div class="input-group mb-2">
-                          <div class="input-group-prepend">
-                            <div class="input-group-text">
-                              <i class="fas fa-search"></i>
+                    <div class="card">
+                      <div class="card-body">
+
+                        <div class="row mb-3">
+                          <div class="col-6 float-left align-middle">
+                            <span>Total de activos: <b>XX</b> </span>
+                          </div>
+                          <div class="col-5 float-right">
+                            <label class="sr-only" for="searchActiveInv">Buscar</label>
+                            <div class="input-group mb-2">
+                              <div class="input-group-prepend">
+                                <div class="input-group-text">
+                                  <i class="fas fa-search"></i>
+                                </div>
+                              </div>
+                              <input type="text" class="form-control" id="searchActiveInv" placeholder="Buscar">
                             </div>
                           </div>
-                          <input type="text" class="form-control" id="searchActiveInv" placeholder="Buscar">
+                        </div>
+
+                        <div class="card collapsed-card">
+                          <div class="card-header text-center filtros-card-background">
+                            <span class="text-white">Filtros</span>
+                            <div class="card-tools">
+                              <button type="button" class="btn btn-tool" data-card-widget="collapse" style="color: white">
+                                <i class="fas fa-plus"></i>
+                              </button>
+                            </div>
+                          </div>
+                          <div class="card-body">
+                            <div class="text-center">
+                              < ------Filtros----- >
+                            </div>
+                          </div>
+                        </div>
+
+                        <div class="mt-3 table-responsive">
+                          <table class="table table-hover">
+                            <thead>
+                              <tr>
+                                <th scope="col">Activo</th>
+                                <th scope="col">Asignación</th>
+                                <th scope="col">Cargado</th>
+                              </tr>
+                            </thead>
+                            <tbody>
+                              <tr>
+                                <td>
+                                  [subtipo de activo]
+                                  <br>
+                                  [tipo de activo]
+                                </td>
+                                <td>
+                                  [asignado]
+                                </td>
+                                <td>
+                                  dd/mm/aa
+                                </td>
+                              </tr>
+                              <tr>
+                                <td>
+                                  [subtipo de activo]
+                                  <br>
+                                  [tipo de activo]
+                                </td>
+                                <td>
+                                  [asignado]
+                                </td>
+                                <td>
+                                  dd/mm/aa
+                                </td>
+                              </tr>
+                              <tr>
+                                <td>
+                                  [subtipo de activo]
+                                  <br>
+                                  [tipo de activo]
+                                </td>
+                                <td>
+                                  [asignado]
+                                </td>
+                                <td>
+                                  dd/mm/aa
+                                </td>
+                              </tr>
+                              <tr>
+                                <td>
+                                  [subtipo de activo]
+                                  <br>
+                                  [tipo de activo]
+                                </td>
+                                <td>
+                                  [asignado]
+                                </td>
+                                <td>
+                                  dd/mm/aa
+                                </td>
+                              </tr>
+                              <tr>
+                                <td>
+                                  [subtipo de activo]
+                                  <br>
+                                  [tipo de activo]
+                                </td>
+                                <td>
+                                  [asignado]
+                                </td>
+                                <td>
+                                  dd/mm/aa
+                                </td>
+                              </tr>
+                            </tbody>
+                          </table>
                         </div>
                       </div>
-                    </div>
-
-                    <button type="button" class="btn btn-primary btn-block" name="button">Filtros</button>
-
-                    <div class="mt-3 table-responsive">
-                      <table class="table table-hover">
-                        <thead>
-                          <tr>
-                            <th scope="col">Activo</th>
-                            <th scope="col">Asignación</th>
-                            <th scope="col">Cargado</th>
-                          </tr>
-                        </thead>
-                        <tbody>
-                          <tr>
-                            <td>
-                              [subtipo de activo]
-                              <br>
-                              [tipo de activo]
-                            </td>
-                            <td>
-                              [asignado]
-                            </td>
-                            <td>
-                              dd/mm/aa
-                            </td>
-                          </tr>
-                          <tr>
-                            <td>
-                              [subtipo de activo]
-                              <br>
-                              [tipo de activo]
-                            </td>
-                            <td>
-                              [asignado]
-                            </td>
-                            <td>
-                              dd/mm/aa
-                            </td>
-                          </tr>
-                          <tr>
-                            <td>
-                              [subtipo de activo]
-                              <br>
-                              [tipo de activo]
-                            </td>
-                            <td>
-                              [asignado]
-                            </td>
-                            <td>
-                              dd/mm/aa
-                            </td>
-                          </tr>
-                          <tr>
-                            <td>
-                              [subtipo de activo]
-                              <br>
-                              [tipo de activo]
-                            </td>
-                            <td>
-                              [asignado]
-                            </td>
-                            <td>
-                              dd/mm/aa
-                            </td>
-                          </tr>
-                          <tr>
-                            <td>
-                              [subtipo de activo]
-                              <br>
-                              [tipo de activo]
-                            </td>
-                            <td>
-                              [asignado]
-                            </td>
-                            <td>
-                              dd/mm/aa
-                            </td>
-                          </tr>
-                        </tbody>
-                      </table>
                     </div>
 
                   </div>
