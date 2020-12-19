41bfb94587205b8d5e97217e26d61a59b9a7d73c
Alfred Ortega <omar.alfredo49@gmail.com>
2020-12-19 17:43:37 -0600

    Fix downs

--- public/resources/js/backoffice/bajas.js
+++ public/resources/js/backoffice/bajas.js
@@ -1,6 +1,34 @@
 var url = $('#url').val( );
 var downTable = null;
 
+let spanish =
+{
+  sProcessing: 'Procesando...',
+  sLengthMenu: 'Mostrar _MENU_ registros',
+  sZeroRecords: 'No se encontraron resultados',
+  sEmptyTable: 'Ningún dato disponible en esta tabla',
+  sInfo: 'Mostrando _START_ - _END_ de _TOTAL_',
+  sInfoEmpty: 'Sin registros',
+  sInfoFiltered: '(filtrado de un total de _MAX_ registros)',
+  sInfoPostFix: '',
+  sSearch: 'Buscar:',
+  sUrl: '',
+  sInfoThousands: ',',
+  sLoadingRecords: 'Cargando...',
+  oPaginate:
+  {
+    sFirst: 'Primero',
+    sLast: 'Último',
+    sNext: 'Siguiente',
+    sPrevious: 'Anterior',
+  },
+  oAria:
+  {
+    sSortAscending: ': Activar para ordenar la columna de manera ascendente',
+    sSortDescending: ': Activar para ordenar la columna de manera descendente',
+  },
+};
+
 function dataURLtoFile( dataurl, filename )
 {
 
