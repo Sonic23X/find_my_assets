fa5e9a1a0add46b0ed9be8197871b2e694b2985e
Alfred Ortega <omar.alfredo49@gmail.com>
2021-03-28 18:09:04 -0600

    Change plans

--- app/Views/landing/planes.php
+++ app/Views/landing/planes.php
@@ -13,20 +13,24 @@
 
             <div class="swiper-slide planes-slide">
               <a href="<?= base_url( 'registro' ) ?>">
-                <img src="/images/landing/planes/plan1.png" alt="Paso 1" class="img-fluid">
+                <img src="/images/landing/planes/plan1.jpg" alt="Paso 1" class="img-fluid">
               </a>
             </div>
             <div class="swiper-slide planes-slide">
               <a href="<?= base_url( 'registro' ) ?>">
-                <img src="/images/landing/planes/plan2.png" alt="Paso 2" class="img-fluid">
+                <img src="/images/landing/planes/plan2.jpg" alt="Paso 2" class="img-fluid">
               </a>
             </div>
             <div class="swiper-slide planes-slide">
               <a href="#contactenos">
-                <img src="/images/landing/planes/plan3.png" alt="Paso 3" class="img-fluid">
+                <img src="/images/landing/planes/plan3.jpg" alt="Paso 3" class="img-fluid">
               </a>
             </div>
-
+            <div class="swiper-slide planes-slide">
+              <a href="#">
+                <img src="/images/landing/planes/plan4.jpg" alt="Paso 4" class="img-fluid">
+              </a>
+            </div>
           </div>
 
           <div class="swiper-button-prev prev-planes"></div>
