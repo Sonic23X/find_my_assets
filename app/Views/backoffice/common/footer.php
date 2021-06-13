9817f05a29d061620261bbbd223ea2108009ad32
Alfred Ortega <omar.alfredo49@gmail.com>
2021-06-12 20:49:46 -0500

    Remove async of script

--- app/Views/backoffice/common/footer.php
+++ app/Views/backoffice/common/footer.php
@@ -67,7 +67,7 @@
     <script src="https://cdn.jsdelivr.net/gh/gitbrent/bootstrap4-toggle@3.6.1/js/bootstrap4-toggle.min.js"></script>
 
     <!-- Select2 -->
-    <script src="<?= base_url( ) ?>/resources/plugins/select2/select2.min.js" async></script>
+    <script src="<?= base_url( ) ?>/resources/plugins/select2/select2.min.js"></script>
 
     <!-- Custom JS -->
     <script src="<?= base_url( ) ?>/resources/js/backoffice/<?= $js ?>.js"></script>
